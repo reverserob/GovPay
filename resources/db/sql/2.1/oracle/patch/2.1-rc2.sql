6a5cdb96858cd3faad9597a6c624ed09ef6c8108
Giovanni Bussu <bussu@link.it>
2016-06-09 15:25:04 +0200

    [GP-260] aggiunti campi dataAcquisizione e dataAcquisizioneRevoca a pagamenti

--- resources/db/sql/2.1/oracle/patch/2.1-rc2.sql
+++ resources/db/sql/2.1/oracle/patch/2.1-rc2.sql
@@ -2,5 +2,5 @@ ALTER TABLE psp MODIFY ragione_sociale VARCHAR(70);
 ALTER TABLE versamenti ADD cod_lotto VARCHAR(35);
 ALTER TABLE versamenti ADD cod_versamento_lotto VARCHAR(35);
 ALTER TABLE versamenti ADD cod_anno_tributario VARCHAR(35);
-ALTER TABLE pagamenti ADD data_acquisizione TIMESTAMP NOT NULL;
+ALTER TABLE pagamenti ADD data_acquisizione TIMESTAMP NOT NULL default CURRENT_TIMESTAMP;
 ALTER TABLE pagamenti ADD data_acquisizione_revoca TIMESTAMP;
--- resources/db/sql/2.1/postgresql/patch/2.1-rc2.sql
+++ resources/db/sql/2.1/postgresql/patch/2.1-rc2.sql
@@ -2,5 +2,5 @@ ALTER TABLE psp ALTER COLUMN ragione_sociale TYPE VARCHAR(70);
 ALTER TABLE versamenti ADD COLUMN cod_lotto VARCHAR(35);
 ALTER TABLE versamenti ADD COLUMN cod_versamento_lotto VARCHAR(35);
 ALTER TABLE versamenti ADD COLUMN cod_anno_tributario VARCHAR(35);
-ALTER TABLE pagamenti ADD COLUMN data_acquisizione TIMESTAMP NOT NULL;
-ALTER TABLE pagamenti ADD COLUMN data_acquisizione_revoca TIMESTAMP NOT NULL;
+ALTER TABLE pagamenti ADD COLUMN data_acquisizione TIMESTAMP NOT NULL DEFAULT current_timestamp;
+ALTER TABLE pagamenti ADD COLUMN data_acquisizione_revoca TIMESTAMP;
